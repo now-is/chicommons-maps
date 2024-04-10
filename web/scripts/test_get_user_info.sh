df04f4387aef11ce44e367f4e2ad7ad3c74fbb4d
Max Graziano <30026209+maxgraziano@users.noreply.github.com>
2024-04-09 19:59:30 -0500

    Cleaning up deprecated files.

--- web/scripts/test_get_user_info.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#curl -i --header "Content-type: application/json" "http://localhost:8000/user_info"
-curl -i --header "Content-type: application/json" --header "Authorization: Token 1647eb098fbde9f19752eaa93c4f3700d8674151" "http://localhost:8000/user_info"
-
--- web/scripts/test_logout.sh
+++
@@ -1,2 +0,0 @@
-curl -v --header "Content-type: application/json" --header "Authorization: Token 2d91632fd0c49aaf161c1bb19754b34972e3b809" --request GET "http://127.0.0.1:8000/logout"
- 
